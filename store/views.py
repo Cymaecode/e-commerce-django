ec8cb4e3d03edc11f34046a1a79257647dae8e75
Cymaecode <cyranmae2026@outlook.com>
2026-02-04 01:49:17 +0700

    feature add home page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+
+# serves the home page to the user
+def home(request):
+    return render(request, 'store/home.html')
